90b2a6052a11b73e203ebc87cf293483360297bb
abdtrust19991 <117579217+abdtrust19991@users.noreply.github.com>
2023-01-30 13:44:36 +0300

    Update purchase.py

--- purchase_delivery_method/models/purchase.py
+++ purchase_delivery_method/models/purchase.py
@@ -21,7 +21,7 @@ class StockRule(models.Model):
     def _prepare_purchase_order(self, company_id, origins, values):
         res = super(StockRule, self)._prepare_purchase_order(company_id, origins, values)
         values = values[0]
-        partner = values["supplier"].purchase_requisition_id.name
+        partner = values["supplier"].partner_id.name
         if partner.purchase_delivery_carrier_id:
             res.update({
                 'carrier_id': partner.purchase_delivery_carrier_id.id
