cd6aa030b1de509f316dfa8c8a6a1a7b8567a62c
abdtrust19991 <117579217+abdtrust19991@users.noreply.github.com>
2024-01-03 23:39:19 -0800

    journal_entry_print
    journal_entry_print

---
+++ tis_journal_entry_print/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd. - ©
+# Technaureus Info Solutions Pvt. Ltd 2020. All rights reserved.
+
+{
+    'name': 'Print Journal Entry',
+    'version': '16.0.0.0',
+    'category': 'Accounting',
+    'sequence': 1,
+    "author": "Technaureus Info Solutions Pvt. Ltd.",
+    "website": "http://www.technaureus.com/",
+    'price': 5,
+    'currency': 'EUR',
+    'license': 'Other proprietary',
+    'depends': [ 'account'],
+    'data': ['report/journal_entry_report.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['images/journal_entry_screenshot.png'],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
