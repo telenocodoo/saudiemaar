955cd7eb8a5defe1037388b34dd483942b25b7b1
nasrom9 <nasrom9@gmail.com>
2025-09-28 09:29:55 +0300

    [FIX] sales report

--- custom_sales_report/report/action_report_custom_sales.py
+++ custom_sales_report/report/action_report_custom_sales.py
@@ -21,14 +21,14 @@ class ReportCustomSales(models.AbstractModel):
             sheet.set_column(col_num, col_num, width)
 
         # Define the headers
-        headers = ['DATE', 'Supplier', 'Customer', 'Salesman', 'Qty / Ton', 'Customer invoice', 'Supllier bill', 'GROSS PROFIT']
+        headers = ['DATE', 'Ref', 'Supplier', 'Customer', 'Salesman', 'Qty / Ton', 'Customer invoice', 'Supllier bill', 'GROSS PROFIT']
         for col_num, header in enumerate(headers):
             sheet.write(0, col_num, header, bold)
             
         row = 1
         for order in sale_orders:
             total_qty = sum(line.product_uom_qty for line in order.order_line)
-            purchase_order_ids = order._get_purchase_orders()
+            purchase_order_ids = order._get_purchase_orders().filtered(lambda po: po.state in ['purchase', 'done'])
             supplier_bills = purchase_order_ids.mapped('invoice_ids').filtered(lambda inv: inv.state == 'posted' and inv.move_type == 'in_invoice')
             customer_invoices = order.invoice_ids.filtered(lambda inv: inv.state == 'posted' and inv.move_type == 'out_invoice')
             total_supplier_bill = sum(bill.amount_total for bill in supplier_bills)
@@ -36,12 +36,13 @@ class ReportCustomSales(models.AbstractModel):
             gross_profit = total_customer_invoice - total_supplier_bill
             
             sheet.write(row, 0, order.date_order.strftime('%Y-%m-%d'))
-            sheet.write(row, 1, order.partner_id.name)
-            sheet.write(row, 2, order.partner_shipping_id.name)
-            sheet.write(row, 3, order.user_id.name)
-            sheet.write(row, 4, total_qty)
-            sheet.write(row, 5, total_customer_invoice)
-            sheet.write(row, 6, total_supplier_bill)
-            sheet.write(row, 7, gross_profit)
+            sheet.write(row, 1, order.name)
+            sheet.write(row, 2, purchase_order_ids.partner_id.name or '')
+            sheet.write(row, 3, order.partner_id.name)
+            sheet.write(row, 4, order.user_id.name)
+            sheet.write(row, 5, total_qty)
+            sheet.write(row, 6, total_customer_invoice)
+            sheet.write(row, 7, total_supplier_bill)
+            sheet.write(row, 8, gross_profit)
             row += 1
         
